57b35382cc989954954941dd9cc5fa7ad997f613
Dimitris Kokkinis <kokkinisd@di.uoa.gr>
2022-02-02 18:37:46 +0200

    Deleted previously commented code

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      {/* <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" /> */}
       <SearchParams />
     </div>
   );
