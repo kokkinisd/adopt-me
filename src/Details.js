421370cc11b9fd6fd4330f0609efe2e3a5950558
Dimitris Kokkinis <kokkinisd@di.uoa.gr>
2022-02-02 20:18:57 +0200

    Class component containing details for each pet

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,37 @@
-const Details = () => {
-  return <h2>to be completed</h2>;
-};
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
 
-export default Details;
+class Details extends Component {
+  constructor() {
+    super();
+    this.state = { loading: true };
+  }
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    ).then((res) => res.json());
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        res.pets[0]
+      )
+    );
+  }
+  render() {
+    const { animal, breed, city, state, description, name } = this.state;
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Details);
